4af3e0a1236a746651590052a674249c063beda1
eldin001 <eldin.balje@icloud.com>
2024-05-06 14:20:52 +0200

    git init

---
+++ data.js
@@ -0,0 +1,22 @@
+export const topics = [
+    {
+        id: 1,
+        text: 'HTML',
+        done: false,
+    },
+    {
+        id: 2,
+        text: 'CSS',
+        done: false,
+    },
+    {
+        id: 3,
+        text: 'JavaScript',
+        done: false,
+    },
+    {
+        id: 4,
+        text: 'VueJs',
+        done: false,
+    },
+]
---
+++ script.js
@@ -0,0 +1,45 @@
+import { topics } from "./data.js";
+
+const {createApp} = Vue;
+
+createApp({
+    data(){
+        return{
+            topics: topics,
+            itemText: '',
+        }
+    },
+    methods: {
+        toggleDone(id){
+            const element = this.topics.find((el)=>{
+                return el.id === id;
+            })
+            if(element) {
+            element.done = !element.done;
+        }
+        },
+        removeItem(id) {
+            const i = this.topics.findIndex((el)=> el.id === id);
+            if(i !== -1) {
+                this.topics.splice(i,1);
+            }
+            
+        },
+        addTodo() {
+            const newObj = {
+                id: null,
+                text: this.itemText,
+                done: false
+            }
+            let nextId = 0;
+            this.topics.forEach((el)=>{
+                if(nextId < el.id) {
+                    nextId = el.id;
+                }
+            });
+            newObj.id = nextId +1;
+            this.topics.push(newObj)
+            
+        }
+    }
+}).mount('#app')
